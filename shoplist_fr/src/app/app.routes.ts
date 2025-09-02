38fd22fcb64a42c41cf06746b85da96e9f1dd708
winwinwai <winwinwai667@gmial.com>
2025-09-02 13:41:23 +0630

    Added needed providers and specified routes

--- shoplist_fr/src/app/app.routes.ts
+++ shoplist_fr/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ItemsComponent } from './components/items/items.component';
 import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { unsavedChangesGuard } from './guard/unsaved-changes.guard';
 
 export const routes: Routes = [
     {
@@ -10,5 +14,19 @@ export const routes: Routes = [
     {
         path: 'items/:itemId',
         component: ItemDetailsComponent
-    }
+    },
+
+    {
+        path: 'register',
+        component: RegisterComponent,
+        canDeactivate: [unsavedChangesGuard]
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    { path: 'profile',
+         component: ProfileComponent,
+         canDeactivate: [unsavedChangesGuard] }
+         
 ];
