43afe1ae71427689950a0a69710ecccd9bb03d46
winwinwai <winwinwai667@gmial.com>
2025-09-02 10:32:18 +0630

    Created nav bar and add some methods

--- shoplist_fr/src/app/components/header/header.component.ts
+++ shoplist_fr/src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { AuthServiceService } from '../../services/auth-service.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule,RouterLink,RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+ constructor(private authService: AuthServiceService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
