6f820ed4dd7f10bc57351d27792479646360adab
winwinwai <winwinwai667@gmail.com>
2025-09-15 10:40:34 +0630

    -changed getter method name f() to formData() in ts file. -change custom validator to email field. -add new condition checking  in canDeactivate() method. -change f to formData, access to form controls in template.

--- shoplist_fr/src/app/components/register/register.component.ts
+++ shoplist_fr/src/app/components/register/register.component.ts
@@ -8,54 +8,55 @@ import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule,NgIf,RouterLinkActive,RouterLink,CommonModule],
+  imports: [ReactiveFormsModule, NgIf, RouterLinkActive, RouterLink, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-   
-   registerForm : FormGroup;
+
+  registerForm: FormGroup;
+  isSubmitted = false;
   showPassword: boolean = false;
- 
-  constructor(private fb: FormBuilder , private router: Router, private userService: UserServiceService) { 
+
+  constructor(private fb: FormBuilder, private router: Router, private userService: UserServiceService) {
 
     this.registerForm = this.fb.group({
-      name : ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
-      email : ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       phone: ['', [Validators.required, Validators.pattern('^[0-9]{9,11}$')]],
       address: ['', [Validators.required]]
     });
   }
-  get f() {
+  get formData() {
     return this.registerForm.controls;
   }
-  
-     togglePassword() {
-  this.showPassword = !this.showPassword;
-}
 
-canDeactivate(): boolean {
-    if (this.registerForm.dirty) {
-      return confirm('You have unsaved changes. Are you sure you want to leave?');
-    }
-    return true;
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
 
-    onSubmit(): void {
+  onSubmit(): void {
     if (this.registerForm.valid) {
       const user: User = this.registerForm.value;
       this.userService.registerUser(user).subscribe({
         next: (savedUser) => {
           alert('User registered successfully: ' + savedUser.name);
-         this.router.navigate(['/login']); 
-          
+          this.isSubmitted = true;
+          this.router.navigate(['/login']);
+
         },
         error: (err) => {
           alert('Error: ' + err.error.message || 'Registration failed!');
         }
       });
     }
+  }
+  canDeactivate(): boolean {
+    if (this.registerForm.dirty && !this.isSubmitted) {
+      return confirm('You have unsaved changes. Are you sure you want to leave?');
+    }
+    return true;
   }
 
 
